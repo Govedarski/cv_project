cebfd0f3c8771f8b3b67817e2b67964f633084fc
Govedarski <velislavgovedarski@gmail.com>
2023-02-06 16:23:32 +0200

    Edit handle_unique_constrain_violation

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,11 +1,11 @@
 import re
 
 from psycopg2.errorcodes import UNIQUE_VIOLATION
+from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import BadRequest
 
 from db import db
-
 
 
 def handle_unique_constrain_violation(func):
@@ -14,24 +14,22 @@ def handle_unique_constrain_violation(func):
             result = func(cls, data, *args, **kwargs)
         except IntegrityError as ex:
             if ex.orig.pgcode == UNIQUE_VIOLATION:
+                ERROR_MESSAGE = "is already taken!"
                 db.session.rollback()
                 errors = {}
-                error_message = "Is already taken!"
                 model = cls.get_model()
                 unique_columns = [column.name for column in model.__table__.columns if column.unique]
-                error_column = re.findall(r'Key \(([^)]+)', ex.orig.pgerror)[0]
-                errors[error_column] = error_message
+                pattern = r'Key \(([^)]+)'
+                error_column = re.findall(pattern, ex.orig.pgerror)[0]
+                errors[error_column] = ERROR_MESSAGE
                 error_column_index = unique_columns.index(error_column)
                 columns_to_check = unique_columns[error_column_index + 1:]
                 for column in columns_to_check:
                     criteria = {column: data[column]}
                     if model.query.filter_by(**criteria).first():
-                        errors[column] = error_message
+                        errors[column] = ERROR_MESSAGE
                 raise BadRequest(errors)
             raise ex
         return result
 
     return wrapper
-
-
-
