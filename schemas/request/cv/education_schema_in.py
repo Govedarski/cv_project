2a7beb078205daa2c14b6547bd478c52cb5b4fb8
Govedarski <velislavgovedarski@gmail.com>
2023-04-19 01:40:20 +0300

    Final fixes

--- schemas/request/cv/education_schema_in.py
+++ schemas/request/cv/education_schema_in.py
@@ -11,7 +11,7 @@ class EducationSchemaIn(Schema):
     fields_of_study = fields.List(fields.Str(), allow_none=True)
     start_date = fields.String(required=True)
     end_date = fields.String(required=True)
-    description = fields.String(validate=validate.Length(max=500))
+    description = fields.String(allow_none=True, validate=validate.Length(max=500))
     diploma_number = fields.String(validate=validate.Length(max=100))
     diploma_binary = fields.String(allow_none=True)
     diploma_extension = fields.String(allow_none=True)
--- schemas/request/cv/work_exp_schema_in.py
+++ schemas/request/cv/work_exp_schema_in.py
@@ -7,7 +7,7 @@ class WorkExpSchemaIn(Schema):
     company_name = fields.String(required=True, validate=validate.Length(max=64))
     job_title = fields.String(required=True, validate=validate.Length(max=64))
     field_of_work = fields.String(required=True, validate=validate.Length(max=64))
-    description = fields.String(validate=validate.Length(max=500))
+    description = fields.String(allow_none=True, validate=validate.Length(max=500))
     employment_type = fields.Enum(EmploymentTypeEnum, required=True, by_value=True)
     start_date = fields.String(required=True)
     end_date = fields.String(required=True)
