2190a844446e4d91d5b6453706b7873d2d679ee3
Govedarski <velislavgovedarski@gmail.com>
2023-04-18 22:48:48 +0300

    Fixed bugs

--- schemas/request/cv/requirement_schema_in.py
+++ schemas/request/cv/requirement_schema_in.py
@@ -5,8 +5,8 @@ from models.enums.cv.employment_type_enum import EmploymentTypeEnum
 
 class RequirementSchemaIn(Schema):
     name = fields.String(required=True)
-    salary_min_range = fields.Integer()
-    salary_max_range = fields.Integer()
+    salary_min_range = fields.Integer(allow_none=True)
+    salary_max_range = fields.Integer(allow_none=True)
     employment_type = fields.Enum(EmploymentTypeEnum,
                                   by_value=True)
 
